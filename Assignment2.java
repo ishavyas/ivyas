6978209fa26e26f3be2f5c9038fadbc6a43c6f8d
ishavyas <isha.vyas4@gmail.com>
2021-01-19 13:50:02 -0500

    Add files via upload

---
+++ Assignment2.java
@@ -0,0 +1,45 @@
+package trainingAssignment1;
+
+class Customer{
+	int custID;
+	static int AgentID;
+	static String AgentName;
+	String Destination;
+	float ticketchrg;	
+}
+
+
+public class Assignment2 {
+	public static void main(String args[]){
+		
+		Customer.AgentID = 45786;
+		Customer.AgentName = "Anne Brooks";
+		
+		Customer Tony = new Customer();
+		Tony.custID = 45714;
+		Tony.Destination = "New York";
+		Tony.ticketchrg = 139.99f;
+		
+		System.out.println("CustomerID:" + Tony.custID);
+		System.out.println("Destination:" + Tony.Destination);
+		System.out.println("AgentID:" + Tony.AgentID);
+		System.out.println("Agent Name:" + Tony.AgentName);
+		System.out.println("Ticket Charge:" + Tony.ticketchrg);
+		System.out.println("=================================");
+		
+		Customer Nancy = new Customer();
+		Nancy.custID = 87695;
+		Nancy.Destination = "Chicago";
+		Nancy.ticketchrg = 219.75f;
+		
+		System.out.println("CustomerID:" + Nancy.custID);
+		System.out.println("Destination:" + Nancy.Destination);
+		System.out.println("AgentID:" + Nancy.AgentID);
+		System.out.println("Agent Name:" + Nancy.AgentName);
+		System.out.println("Ticket Charge:" + Nancy.ticketchrg);
+		System.out.println("=================================");
+		
+		
+	}
+
+}
---
+++ trainingAssignment1.java
@@ -0,0 +1,42 @@
+package trainingAssignment1;
+
+class student{
+	int bookid;
+	String bookName;
+	static String schoolName;
+	static int schoolID;
+	float latecharge;
+	
+}
+
+public class trainingAssignment1 {
+	public static void main(String args[]) {
+		
+		student.schoolName = "St.James";
+		student.schoolID =1044;
+		
+		 student John = new student();
+		John.bookid =1476;
+		John.bookName = "fundamentals of chemistry";
+		John.latecharge = 1.99f;
+		
+		System.out.println("BookId:" + John.bookid);
+		System.out.println("BookName:" + John.bookName);
+		System.out.println("SchoolName:" + John.schoolName);
+		System.out.println("SchoolID:"+ John.schoolID);
+		System.out.println("Late Charge:" + John.latecharge);
+		System.out.println("===================");
+		
+		student Jane = new student();
+		Jane.bookid = 41785;
+		Jane.bookName = "Basics of French";
+		Jane.latecharge = 3.99f; 
+		System.out.println("BookID:" + Jane.bookid);
+		System.out.println("BookName:" + Jane.bookName);
+		System.out.println("SchoolName:" + Jane.schoolName);
+		System.out.println("SchoolID:" + Jane.schoolID);
+		System.out.println("Late Charge:" + Jane.latecharge);
+		System.out.println("===================");
+	
+	}
+}
