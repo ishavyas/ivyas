825674dd50cdb3479d097b5b2932afc598dfaabd
ishavyas <isha.vyas4@gmail.com>
2021-01-20 00:04:25 -0500

    Add files via upload

---
+++ assign1.java
@@ -0,0 +1,31 @@
+package trainingAssignment2;
+
+
+class library{
+	void booklist() {
+		System.out.println("1.Fundamentals of Chemistry 2. Fun with Maths 3. History Facts and More");
+	}
+	void orderBooks(String book, int qty) {
+		System.out.println("Book ordered is: " + book + " in quantity: " +qty);
+	}
+	String deliveredBooks() {
+		return "Fun with Maths";
+	}
+	String payment(int amt, int qty) {
+		System.out.println("Payment is: " +(amt*qty));
+		return "recieved";
+	}
+}
+
+
+public class assign1 {
+	public static void main(String args[]) {
+		library StJohn = new library();
+		StJohn.booklist();
+		StJohn.orderBooks("Fun with Maths", 5);
+		String bookName = StJohn.deliveredBooks();
+		System.out.println("Book Delivered is:" +bookName);
+		System.out.println("Payment " + StJohn.payment(10, 5));		
+	}
+
+}
---
+++ assign2.java
@@ -0,0 +1,33 @@
+package trainingAssignment2;
+
+class tuition {
+	void subjects() {
+		System.out.println("Package available: 1.Silver- Beginners, 2.Golden- Intermediate, 3.Platinum - Advanced ");
+		System.out.println("Subjects available: 1.Java for Beginners, 2.Java for intermediate, 3.Advanced Application Development");
+	}
+	void subchosen(String sub) {
+		System.out.println("Subject chosen by the student is: " +sub);
+	}
+	String pkgalotment() {
+		return "Silver for Java for Beginners";
+	}
+	String Fees(int fee) {
+		System.out.println("Fee for the course is: $" +fee);
+		return "paid";
+	}
+}
+
+public class assign2 {
+	public static void main(String args[]) {
+		tuition programmasters = new tuition();
+		programmasters.subjects();
+		programmasters.subchosen("Java for Beginners");
+		String pkg = programmasters.pkgalotment();
+		System.out.println("Package alotted is: " +pkg);
+		System.out.println("Fee " +programmasters.Fees(500));
+		
+		
+		
+	}
+
+}
