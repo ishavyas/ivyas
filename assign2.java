e11c0ae9c66fa047bac5433411c8081d3b8c2efd
ishavyas <isha.vyas4@gmail.com>
2021-01-22 00:38:10 -0500

    Update assign2.java

--- assign2.java
+++ assign2.java
@@ -19,7 +19,7 @@ class tuition {
 	}
 }
 
-public class assign2 {
+public class variables2 {
 	public static void main(String args[]) {
 		tuition programmasters = new tuition();
 		programmasters.subjects();
