c4f065d5e2c7f77c94c357502528f3df7f3d2f29
ishavyas <isha.vyas4@gmail.com>
2021-02-03 00:47:19 -0500

    Create Veena.java

---
+++ Assignment1/Musical/Music.String/Veena.java
@@ -0,0 +1,12 @@
+package Music.string;
+import Music.*;
+
+public class Veena implements Playable {
+
+	@Override
+	public void play() {
+		System.out.println("Veena is a musical instrument played by moving fingers on the strings present on it.");
+		System.out.println("Beautiful tunes are being played on the Veena");
+	}
+	
+}
