15f01dd3ffc4ed7e02817dc9ef2e7780b0174ea8
ishavyas <isha.vyas4@gmail.com>
2021-02-03 00:47:56 -0500

    Create Saxophone.java

---
+++ Assignment1/Musical/Music.Wind/Saxophone.java
@@ -0,0 +1,14 @@
+package Music.Wind;
+
+import Music.*;
+
+public class Saxophone implements Playable {
+
+	@Override
+	public void play() {
+		System.out.println("Saxophone is an instrument played by blowing air into it.");
+		System.out.println("Saxophone is being played beautifully.");
+		
+	}
+
+}
