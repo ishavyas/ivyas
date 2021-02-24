c49f7f51ada3be47e72662e9884549ddfa85dd24
ishavyas <isha.vyas4@gmail.com>
2021-02-24 01:14:17 -0500

    Update FileDemo.java

--- FileOutput/FileDemo.java
+++ FileOutput/FileDemo.java
@@ -8,7 +8,10 @@ public class FileDemo {
 
 	public static void main(String[] args) throws IOException {
 		FileOutputStream fos = new FileOutputStream("D:\\EmpFiles\\demo.txt");
+		String s = "This is a file demo";
+		byte [] b = s.getBytes();
 		fos.write(70);
+		fos.write(b);
 		fos.close();
 
 	}
